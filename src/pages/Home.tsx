05e70b3f34c24f9bd8a585080fa3613ba2702f89
Chigusa-Sato <chigusa.sato@rakus-partners.co.jp>
2022-08-02 22:54:05 +0900

    ダッシュボード画面　雛形作成

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,8 @@
-import { Link } from 'react-router-dom';
-import Button from '../components/atoms/Button';
+import DashBoard from '../pages/DashBoard';
 const Home = () => {
   return (
     <>
-      <h1>ホーム</h1>
-      <Button text="サンプル" />
-      <div>
-        新規登録は<Link to={`/register/`}>こちら</Link>
-      </div>
+      <DashBoard />
     </>
   );
 };
