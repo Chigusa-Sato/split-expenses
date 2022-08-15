fedea7dd91026d7487f94fa6b9e694100a35d734
Chigusa-Sato <chigusa.sato@rakus-partners.co.jp>
2022-08-16 00:02:30 +0900

    合計金額をストアに保存

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,5 @@
 import { combineReducers } from 'redux';
 import counter from './counter';
+import price from './price';
 
-export default combineReducers({ counter });
+export default combineReducers({ counter, price });
